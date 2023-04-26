5a703d22b8a69bc96b98415860bf66723f0086cf
TuanNV <tuannv@miichisoft.net>
2023-04-26 09:49:48 +0700

    fix conflict

--- src/components/PopupCommon/index.tsx
+++ src/components/PopupCommon/index.tsx
@@ -10,7 +10,6 @@ import CreateUser from "@/modules/User/components/CreateUser"
 import CreateCustomer from "@/modules/Customer/components/CreateCustomer"
 import ChangePassword from "@/modules/UserDetail/components/ChangePassword"
 import EditUser from "@/modules/UserDetail/components/EditUser"
-import CreateCustomer from "@/modules/Customer/components/CreateCustomer"
 
 const PopupCommon = () => {
     const dispatch = useDispatch()
